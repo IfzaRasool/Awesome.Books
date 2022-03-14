08e28b850526236367b930dbf02cf93e63721c57
maxthestranger <thestrangermax@gmail.com>
2022-03-14 18:18:36 +0300

    code refactor

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
-const inputTitle=document.querySelector("#title");
-const inputauthor=document.querySelector("#author");
-const Books=document.querySelector("#books > ul");
-const AddBtn=document.querySelector(".add-btn");
+const bookDiv = document.querySelector('#books > ul');
+const form = document.querySelector('.add-book');
 
-AddBtn.addEventListener("click", createItem);
-const myArray =[];
+// list of books
+const bookList = JSON.parse(localStorage.getItem('book_info'));
 
+// read book item
+function readValue() {
+  bookDiv.innerHTML = '';
 
-function createItem() {
-    myArray.push({
-        title_name: document.getElementById('title').value,
-        author_name: document.getElementById('author').value,
+  if (bookList) {
+    bookList.forEach((book, index) => {
+      const li = document.createElement('li');
+      li.innerHTML = `<span>${book.title_name}</span><br> 
+         <span>${book.author_name}</span><br>
+         <button data-id='${index}'>remove</button><hr>`;
+
+      bookDiv.appendChild(li);
     });
-    localStorage.setItem("book_info",JSON.stringify(myArray));
-    readValue();
   }
+}
 
-  function readValue() {
-    var x = JSON.parse(localStorage.getItem("book_info"));
-    console.log(x);
-    x.forEach(element => {
-       let li = document.createElement("li");
-       li.innerHTML=`<span>${element.title_name}</span><br> 
-       <span>${element.author_name}</span><br>
-       <button>remove</button><hr>`
-       Books.appendChild(li);   
-    });
-   
-  }
+// create book item
+function addBook(e) {
+  e.preventDefault();
+  bookList.push({
+    title_name: form.elements.title.value,
+    author_name: form.elements.author.value,
+  });
+
+  localStorage.setItem('book_info', JSON.stringify(bookList));
   readValue();
- 
+}
+
+form.addEventListener('submit', (e) => addBook(e));
+
+readValue();
