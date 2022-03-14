acb2cd248dd545bc8b1e4818b1843e527a43b661
maxthestranger <thestrangermax@gmail.com>
2022-03-14 22:09:33 +0300

    add remove function

--- app.js
+++ app.js
@@ -2,23 +2,25 @@ const bookDiv = document.querySelector('#books > ul');
 const form = document.querySelector('.add-book');
 
 // list of books
-const bookList = JSON.parse(localStorage.getItem('book_info'));
+let bookList = JSON.parse(localStorage.getItem('book_info'));
 
-// read book item
-function readValue() {
+// read books to view
+function readValue(books) {
   bookDiv.innerHTML = '';
 
-  if (bookList) {
-    bookList.forEach((book, index) => {
+  if (books) {
+    books.forEach((book, index) => {
       const li = document.createElement('li');
       li.innerHTML = `<span>${book.title_name}</span><br> 
          <span>${book.author_name}</span><br>
-         <button data-id='${index}'>remove</button><hr>`;
+         <button class="remove-btn" data-id="${index}">remove</button><hr>`;
 
       bookDiv.appendChild(li);
     });
   }
 }
+
+readValue(bookList);
 
 // create book item
 function addBook(e) {
@@ -28,10 +30,28 @@ function addBook(e) {
     author_name: form.elements.author.value,
   });
 
+  // update local storage
   localStorage.setItem('book_info', JSON.stringify(bookList));
-  readValue();
+  readValue(bookList);
+
+  // clear form
+  form.elements.title.value = '';
+  form.elements.author.value = '';
 }
 
+// remove book
+function removeBook(btn) {
+  bookList = bookList.filter((book, index) => index !== Number(btn.dataset.id));
+
+  localStorage.setItem('book_info', JSON.stringify(bookList));
+  readValue(bookList);
+}
+
+// event listeners
 form.addEventListener('submit', (e) => addBook(e));
 
-readValue();
+bookDiv.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove-btn')) {
+    removeBook(e.target);
+  }
+});
